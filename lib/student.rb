811af7c3dd0e3050533f1aab015608bd1e8dd3cf
+ <+>
2022-07-20 18:27:24 -0400

    commit tree 7dde91088ee42fafd95c0d2ceb2ac92dbab50ed0

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,20 @@
-class Student 
+require_relative "./user.rb"
+
+class Student < User
+
+    attr_accessor :knowledge
+
+    def initialize
+        @knowledge = []
+    end
+
+    def learn(thoughts)
+        @knowledge << thoughts
+    end
+
+    def knowledge
+        @knowledge
+    end
+
 
 end
